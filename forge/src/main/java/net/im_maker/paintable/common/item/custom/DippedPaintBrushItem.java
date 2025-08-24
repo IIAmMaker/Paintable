7de4f08ebfd3d7c56023e583efb6db17e1e5d6d8
IIAmMaker <foxy440000@gmail.com>
2025-08-24 23:18:35 +0300

    Updated DippedPaintBrushItem

--- forge/src/main/java/net/im_maker/paintable/common/item/custom/DippedPaintBrushItem.java
+++ forge/src/main/java/net/im_maker/paintable/common/item/custom/DippedPaintBrushItem.java
@@ -2,7 +2,7 @@ package net.im_maker.paintable.common.item.custom;
 
 import net.im_maker.paintable.common.item.PItems;
 import net.im_maker.paintable.common.util.PTags;
-import net.im_maker.paintable.config.PaintableServerConfigs;
+import net.im_maker.paintable.config.PaintableConfig;
 import net.minecraft.advancements.CriteriaTriggers;
 import net.minecraft.core.BlockPos;
 import net.minecraft.core.Direction;
@@ -10,6 +10,8 @@ import net.minecraft.core.Holder;
 import net.minecraft.core.registries.BuiltInRegistries;
 import net.minecraft.core.registries.Registries;
 import net.minecraft.nbt.CompoundTag;
+import net.minecraft.network.chat.Component;
+import net.minecraft.network.chat.Style;
 import net.minecraft.resources.ResourceLocation;
 import net.minecraft.server.level.ServerPlayer;
 import net.minecraft.sounds.SoundEvents;
@@ -21,6 +23,7 @@ import net.minecraft.world.entity.player.Player;
 import net.minecraft.world.item.DyeColor;
 import net.minecraft.world.item.Item;
 import net.minecraft.world.item.ItemStack;
+import net.minecraft.world.item.TooltipFlag;
 import net.minecraft.world.item.context.UseOnContext;
 import net.minecraft.world.level.Level;
 import net.minecraft.world.level.block.BedBlock;
@@ -32,12 +35,14 @@ import net.minecraft.world.level.block.state.BlockState;
 import net.minecraft.world.level.block.state.properties.BedPart;
 import net.minecraft.world.level.block.state.properties.BlockStateProperties;
 import net.minecraft.world.level.block.state.properties.DoubleBlockHalf;
+import org.jetbrains.annotations.Nullable;
 
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 
 public class DippedPaintBrushItem extends Item {
+    private static final String NBT_PAINT = "paint";
     private final DyeColor color;
 
     public DippedPaintBrushItem(DyeColor paintColor, Properties properties) {
@@ -45,96 +50,128 @@ public class DippedPaintBrushItem extends Item {
         this.color = paintColor;
     }
 
-    @Override
-    public boolean hasCraftingRemainingItem(ItemStack stack) {
-        return true;
+    private int getPaintMax() {
+        return PaintableConfig.PAINT_LIMIT.get(); // safe now, called at runtime
+    }
+
+    private void ensurePaintInitialized(ItemStack stack) {
+        CompoundTag tag = stack.getOrCreateTag();
+        if (!tag.contains(NBT_PAINT)) {
+            setPaintCount(stack, getPaintMax());
+        }
+    }
+
+    public int getPaintLeft(ItemStack stack) {
+        ensurePaintInitialized(stack);
+        return stack.getOrCreateTag().getInt(NBT_PAINT);
+    }
+
+    public void setPaintCount(ItemStack stack, int count) {
+        CompoundTag tag = stack.getOrCreateTag();
+        tag.putInt(NBT_PAINT, Math.max(0, Math.min(count, getPaintMax())));
+        stack.setTag(tag);
+    }
+
+    private boolean decrementOrReplace(ItemStack stack, Player player, UseOnContext ctx, Level world) {
+        if (player.isCreative()) return false;
+        int left = getPaintLeft(stack);
+        if (left > 1) {
+            setPaintCount(stack, left - 1);
+            return false;
+        } else {
+            player.setItemInHand(ctx.getHand(), new ItemStack(PItems.PAINT_BRUSH.get()));
+            world.playSound(null, player.blockPosition(), SoundEvents.ITEM_PICKUP, SoundSource.PLAYERS, 1.0F, 1.0F);
+            return true;
+        }
     }
 
     @Override
-    public boolean isEnchantable(ItemStack stack) {
-        return false;
+    public ItemStack getDefaultInstance() {
+        ItemStack s = super.getDefaultInstance();
+        setPaintCount(s, getPaintMax());
+        return s;
     }
 
     @Override
-    public boolean isBookEnchantable(ItemStack stack, ItemStack book) {
-        return false;
-    }
-
-    @Override
-    public boolean isRepairable(ItemStack itemstack) {
-        return false;
+    public void appendHoverText(ItemStack itemStack, @Nullable Level level, List<Component> tooltips, TooltipFlag tooltipFlag) {
+        int current = getPaintLeft(itemStack);
+        tooltips.add(
+                Component.literal("Paint Left: " + current + "/" + getPaintMax())
+                        .withStyle(Style.EMPTY.withColor(color.getMapColor().col))
+        );
+        super.appendHoverText(itemStack, level, tooltips, tooltipFlag);
     }
 
     private void paint(String blockName, ItemStack itemStack, Player player, UseOnContext context, Level world, BlockPos blockPos, BlockState blockState) {
-        paint(blockName, "", "paintable", itemStack, player, context, world, blockPos, blockState, true, true, true);
+        paint(blockName, "", "paintable", itemStack, player, context, world, blockPos, blockState, true, true);
     }
 
-    private void paint(String blockName, String prefix, String blockNameSpace, ItemStack itemStack, Player player, UseOnContext context, Level world, BlockPos blockPos, BlockState blockState, boolean i, boolean t, boolean canRepaint) {
+    private void paint(String blockName, String prefix, String blockNameSpace, ItemStack itemStack, Player player, UseOnContext context, Level world, BlockPos blockPos, BlockState blockState, boolean t, boolean canRepaint) {
+
         if (player instanceof ServerPlayer) {
             CriteriaTriggers.ITEM_USED_ON_BLOCK.trigger((ServerPlayer) player, blockPos, itemStack);
         }
-        if (itemStack.getDamageValue() != 15) {
-            itemStack.hurtAndBreak(1, player, (player1) -> player1.broadcastBreakEvent(context.getHand()));
-        } else {
-            player.setItemInHand(context.getHand(), new ItemStack(PItems.PAINT_BRUSH.get()));
-            world.playSound(null, player.blockPosition(), SoundEvents.ITEM_PICKUP, SoundSource.PLAYERS, 1.0F, 1.0F);
-        }
-        Boolean n = false;
-        BlockPos newBlockPos = blockPos;
-        BlockState newBlockState = blockState;
+
+        decrementOrReplace(itemStack, player, context, world);
+
+        BlockPos otherPos = blockPos;
+        BlockState otherState = blockState;
         if (blockState.getBlock() instanceof BedBlock && canRepaint) {
-            Direction d = blockState.getValue(BlockStateProperties.HORIZONTAL_FACING);
-            BedPart p = blockState.getValue(BlockStateProperties.BED_PART);
-            n = true;
-            newBlockPos = p == BedPart.FOOT ? blockPos.relative(d) : blockPos.relative(d.getOpposite());
-            //newBlockState = world.getBlockState(newBlockPos); //For some reason, it doesn't work like this, but it does with this:
-            BedPart newPart = p == BedPart.FOOT ? BedPart.HEAD : BedPart.FOOT;
-            newBlockState = world.getBlockState(newBlockPos).setValue(BlockStateProperties.BED_PART, newPart);
+            otherPos = handleBed(blockPos, blockState, world);
+            otherState = world.getBlockState(otherPos);
+        } else if (blockState.getBlock() instanceof DoorBlock && canRepaint) {
+            otherPos = handleDoor(blockPos, blockState, world);
+            otherState = world.getBlockState(otherPos);
         }
-        if (blockState.getBlock() instanceof DoorBlock && canRepaint) {
-            DoubleBlockHalf p = blockState.getValue(BlockStateProperties.DOUBLE_BLOCK_HALF);
-            n = true;
-            newBlockPos = p == DoubleBlockHalf.LOWER ? blockPos.above() : blockPos.below();
-            //newBlockState = world.getBlockState(newBlockPos); //For some reason, it doesn't work like this, but it does with this:
-            DoubleBlockHalf newPart = p == DoubleBlockHalf.LOWER ? DoubleBlockHalf.UPPER : DoubleBlockHalf.LOWER;
-            newBlockState = world.getBlockState(newBlockPos).is(BlockTags.WOODEN_DOORS) ? world.getBlockState(newBlockPos).setValue(BlockStateProperties.DOUBLE_BLOCK_HALF, newPart) : world.getBlockState(newBlockPos);
-        }
-        if (i) {
-            ResourceLocation blockLocation = t ?
-                    new ResourceLocation(blockNameSpace + ":" + (blockName.contains("%s") ? String.format(blockName, color + "_painted_") : color + "_painted_" + blockName)) :
-                    new ResourceLocation(blockNameSpace + ":" + prefix + color + "_" + blockName);
-            Block block = BuiltInRegistries.BLOCK.get(blockLocation);
-            if (block != null) {
-                if (!blockState.hasBlockEntity()) {
-                    world.setBlock(newBlockPos, Blocks.AIR.defaultBlockState(), Block.UPDATE_KNOWN_SHAPE);
+
+        ResourceLocation blockLocation = t ?
+                new ResourceLocation(blockNameSpace + ":" + (blockName.contains("%s") ? String.format(blockName, color + "_painted_") : color + "_painted_" + blockName)) :
+                new ResourceLocation(blockNameSpace + ":" + prefix + color + blockName);
+        Block block = BuiltInRegistries.BLOCK.get(blockLocation);
+        if (block != null) {
+            if (!blockState.hasBlockEntity()) {
+                world.setBlock(otherPos, Blocks.AIR.defaultBlockState(), Block.UPDATE_KNOWN_SHAPE);
+                world.setBlockAndUpdate(blockPos, block.withPropertiesOf(blockState));
+                if (!otherPos.equals(blockPos)) world.setBlockAndUpdate(otherPos, block.withPropertiesOf(otherState));
+            } else {
+                BlockEntity be = world.getBlockEntity(blockPos);
+                if (be != null) {
+                    CompoundTag data = be.saveWithFullMetadata();
+                    world.setBlock(otherPos, Blocks.AIR.defaultBlockState(), Block.UPDATE_KNOWN_SHAPE);
                     world.setBlockAndUpdate(blockPos, block.withPropertiesOf(blockState));
-                    if (n) world.setBlockAndUpdate(newBlockPos, block.withPropertiesOf(newBlockState));
-                } else {
-                    BlockEntity blockEntity = world.getBlockEntity(blockPos);
-                    if (blockEntity != null) {
-                        CompoundTag data = blockEntity.saveWithFullMetadata();
-                        world.setBlock(newBlockPos, Blocks.AIR.defaultBlockState(), Block.UPDATE_KNOWN_SHAPE);
-                        world.setBlockAndUpdate(blockPos, block.withPropertiesOf(blockState));
-                        if (n) world.setBlockAndUpdate(newBlockPos, block.withPropertiesOf(newBlockState));
-                        BlockEntity newBlockEntity = world.getBlockEntity(blockPos);
-                        if (newBlockEntity != null) {
-                            newBlockEntity.load(data);
-                            world.setBlockEntity(newBlockEntity);
-                        }
+                    if (!otherPos.equals(blockPos)) world.setBlockAndUpdate(otherPos, block.withPropertiesOf(otherState));
+                    BlockEntity newBe = world.getBlockEntity(blockPos);
+                    if (newBe != null) {
+                        newBe.load(data);
+                        world.setBlockEntity(newBe);
                     }
                 }
-                world.playSound(null, blockPos, SoundEvents.MUD_PLACE, SoundSource.PLAYERS, 1.0F, 1.0F);
             }
+            world.playSound(null, blockPos, SoundEvents.MUD_PLACE, SoundSource.PLAYERS, 1.0F, 0.7F);
         }
     }
 
-    private void cleanPaintBrush (Level world,BlockPos blockpos, UseOnContext context) {
+    private BlockPos handleBed(BlockPos pos, BlockState state, Level world) {
+        Direction d = state.getValue(BlockStateProperties.HORIZONTAL_FACING);
+        BedPart p = state.getValue(BlockStateProperties.BED_PART);
+        return p == BedPart.FOOT ? pos.relative(d) : pos.relative(d.getOpposite());
+    }
+
+    private BlockPos handleDoor(BlockPos pos, BlockState state, Level world) {
+        DoubleBlockHalf half = state.getValue(BlockStateProperties.DOUBLE_BLOCK_HALF);
+        return half == DoubleBlockHalf.LOWER ? pos.above() : pos.below();
+    }
+
+    private void cleanPaintBrush(Level world, BlockPos blockpos, UseOnContext context) {
         context.getPlayer().setItemInHand(context.getHand(), new ItemStack(PItems.PAINT_BRUSH.get()));
-        world.playSound((Player)null, blockpos, SoundEvents.MUD_HIT, SoundSource.PLAYERS, 1.0F, 1.0F);
+        world.playSound((Player) null, blockpos, SoundEvents.MUD_HIT, SoundSource.PLAYERS, 1.0F, 0.7F);
     }
 
     private boolean cantPaint(String block) {
-        return block.contains(":" + color + "_") || block.contains("_" + color + "_") || block.contains("/" + color + "_");
+        boolean flag1 = block.contains(":" + color + "_") || block.contains("/" + color + "_") || block.contains("_" + color) || block.contains("_" + color + "_");
+        boolean flag2 = block.contains(":light_" + color + "_") || block.contains("/light_" + color + "_") || block.contains("light_" + color) || block.contains("_light_" + color + "_");
+        boolean i = block.contains("light_" + color);
+        return !i ? flag1 : !flag2;
     }
 
     @Override
@@ -146,7 +183,8 @@ public class DippedPaintBrushItem extends Item {
         Player player = context.getPlayer();
         String blockID = BuiltInRegistries.BLOCK.getKey(blockState.getBlock()).toString();
 
-        if (cantPaint(blockID) || ((blockState.is(PTags.Blocks.TOGGLEABLE) && PaintableServerConfigs.CAN_PAINT.get()))) return InteractionResult.PASS;
+        if (cantPaint(blockID) || ((blockState.is(PTags.Blocks.TOGGLEABLE) && !PaintableConfig.CAN_PAINT.get())))
+            return InteractionResult.PASS;
 
         if (blockState.is(PTags.Blocks.PAINT_BRUSH_WASHING_BLOCKS)) {
             cleanPaintBrush(world, blockPos, context);
@@ -155,68 +193,66 @@ public class DippedPaintBrushItem extends Item {
 
         for (TagKey<Block> tag : blockState.getTags().toList()) {
             ResourceLocation tagLocation = tag.location();
-            if (tagLocation.getPath().contains("paintable/") && !tagLocation.getPath().contains("paintable/toggleable")) {
-                String path = tagLocation.getPath();
-                String blockName;
-                String prefix;
-                if (path.contains("_c_")) {
-                    String[] parts = path.split("_c_");
-                    prefix = parts[0].replace("paintable/", "") + "_";
+            String path = tagLocation.getPath();
+            if (path.contains("paintable/") && !path.contains("paintable/toggleable")) {
+                String blockName = "";
+                String prefix = "";
+                if (path.contains("__")) {
+                    String[] parts = path.split("__", 2);
+                    prefix = parts[0].replace("paintable/", "");
                     blockName = parts[1].replace("paintable/", "");
+                } else if (path.endsWith("_")) {
+                    // ex "mod99:stone_yellow" -> "stone"
+                    String base = path.replace("paintable/", "");
+                    int lastUnderscore = base.lastIndexOf("_");
+                    if (lastUnderscore != -1) {
+                        prefix = base.substring(0, lastUnderscore);
+                        blockName = "";
+                    }
                 } else {
                     prefix = "";
                     blockName = path.replace("paintable/", "");
                 }
+
+                blockName = blockName.equals("") ? "" : "_" + blockName;
+                prefix = prefix.equals("") ? "" : prefix + "_";
+
                 String nameSpace = tagLocation.getNamespace();
                 TagKey<Block> blockTag = TagKey.create(Registries.BLOCK, tagLocation);
+
                 List<Block> blocks = BuiltInRegistries.BLOCK.getTag(blockTag).stream().flatMap(holder -> holder.stream()).map(Holder::value).toList();
-                boolean cantPaint = true;
-                for (Block block : blocks) {
-                    ResourceLocation blockId = BuiltInRegistries.BLOCK.getKey(block);
-                    if (blockId != null && blockId.getPath().contains("magenta")) {
-                        nameSpace = blockId.getNamespace();
-                        Block blockTest = BuiltInRegistries.BLOCK.get(new ResourceLocation(nameSpace, prefix + color + "_" + blockName));
+
+                boolean foundPaintable = false;
+                for (Block b : blocks) {
+                    ResourceLocation id = BuiltInRegistries.BLOCK.getKey(b);
+                    if (id != null && id.getPath().contains("magenta")) {
+                        nameSpace = id.getNamespace();
+                        Block blockTest = BuiltInRegistries.BLOCK.get(new ResourceLocation(nameSpace, prefix + color + blockName));
+                        //System.out.println(blockTest);
+
                         if (blockTest != Blocks.AIR) {
-                            cantPaint = false;
+                            foundPaintable = true;
                             break;
                         }
-                    } else if (blockId != null && blockId.getPath().contains("maroon")) {
-                        nameSpace = blockId.getNamespace();
-                        cantPaint = false;
+                    } else if (id != null && id.getPath().contains("maroon")) {
+                        nameSpace = id.getNamespace();
+                        foundPaintable = true;
                         break;
                     }
                 }
-                if (cantPaint) return InteractionResult.PASS;
-                Block block = BuiltInRegistries.BLOCK.get(new ResourceLocation(nameSpace, color + "_" + blockName));
-                if (block == null) return InteractionResult.PASS;
-                System.out.println(blockName);
-                System.out.println(prefix);
-                System.out.println(prefix);
-                paint(blockName, prefix, nameSpace, itemStack, player, context, world, blockPos, blockState, true, false, true);
+
+                if (!foundPaintable) return InteractionResult.PASS;
+
+                Block block = BuiltInRegistries.BLOCK.get(new ResourceLocation(nameSpace, prefix + color + blockName));
+                if (block == null || block == Blocks.AIR) return InteractionResult.PASS;
+
+                paint(blockName, prefix, nameSpace, itemStack, player, context, world, blockPos, blockState, false, true);
                 return InteractionResult.sidedSuccess(world.isClientSide);
             }
         }
 
         Map<TagKey<Block>, String> tagMap = new HashMap<>();
-        //tagMap.put(BlockTags.PLANKS, "planks");
-        //tagMap.put(BlockTags.WOODEN_STAIRS, "stairs");
-        //tagMap.put(BlockTags.WOODEN_SLABS, "slab");
-        //tagMap.put(BlockTags.WOODEN_FENCES, "fence");
-        //tagMap.put(BlockTags.FENCE_GATES, "fence_gate");
-        //tagMap.put(BlockTags.WOODEN_BUTTONS, "button");
-        //tagMap.put(BlockTags.WOODEN_PRESSURE_PLATES, "pressure_plate");
-        //tagMap.put(BlockTags.STANDING_SIGNS, "sign");
-        //tagMap.put(BlockTags.WALL_SIGNS, "wall_sign");
-        //tagMap.put(BlockTags.CEILING_HANGING_SIGNS, "hanging_sign");
-        //tagMap.put(BlockTags.WALL_HANGING_SIGNS, "wall_hanging_sign");
-        //tagMap.put(ModBlockTags.PAINTABLE_BRICKS, "bricks");
-        //tagMap.put(ModBlockTags.PAINTABLE_BRICK_STAIRS, "brick_stairs");
-        //tagMap.put(ModBlockTags.PAINTABLE_BRICK_SLABS, "brick_slab");
-        //tagMap.put(ModBlockTags.PAINTABLE_BRICK_WALLS, "brick_wall");
-        //tagMap.put(ModBlockTags.PAINTABLE_MUD_BRICKS, "mud_bricks");
-        //tagMap.put(ModBlockTags.PAINTABLE_MUD_BRICK_STAIRS, "mud_brick_stairs");
-        //tagMap.put(ModBlockTags.PAINTABLE_MUD_BRICK_SLABS, "mud_brick_slab");
-        //tagMap.put(ModBlockTags.PAINTABLE_MUD_BRICK_WALLS, "mud_brick_wall");
+        // (your tagMap entries commented out - keep them if you want)
 
         for (Map.Entry<TagKey<Block>, String> entry : tagMap.entrySet()) {
             if (blockState.is(entry.getKey())) {
